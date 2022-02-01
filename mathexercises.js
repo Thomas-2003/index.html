9518d24525a522d6a6789f3c15df061644aced6b
Thomas-2003 <thomasroenneper12@web.de>
2022-02-01 09:17:39 +0100

    mathexercises

---
+++ mathexercises.js
@@ -0,0 +1,16 @@
+var num1 = 25
+var num2 = "25"
+var type = typeof num2
+// if (25 === num2) { //prefer alwas using 3 equal (===) signs for value and type checking
+//     console.log("True!!!!");
+// } else {
+//     console.log("False :(");
+// }
+
+// var i = 7
+// console.log(i !== 7);
+// var username = "tommy"
+// console.log(username !== "tommy");
+
+var i = 17
+console.log(i >= 18);
