54a259eae18d686d5dfce6f1077651218892bf5b
fraankrr <rr.frank@web.de>
2022-02-01 10:16:07 +0100

    basics functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+console.log("YYYY")
+function product(x,y){
+    return x * y
+}
+console.log(product(444, 2))
+console.log(product(2, 8))
+console.log(product(444, 22222))
+console.log(product(4222244, -7))
+
+const power = (x,y) => {
+  return  Math.pow(x,y)
+}
+console.log(power(2,8))
