8cb885778bb402b4760fe17138916ef9a10a37af
fraankrr <rr.frank@web.de>
2022-02-08 09:59:08 +0100

    pick randoms person

--- mathexercises.js
+++ mathexercises.js
@@ -1,6 +1,6 @@
-var num1 = 25
-var num2 = "25"
-var type = typeof num2
+// var num1 = 25
+// var num2 = "25"
+// var type = typeof num2
 // if (25 === num2) { //prefer alwas using 3 equal (===) signs for value and type checking
 //     console.log("True!!!!");
 // } else {
@@ -12,5 +12,20 @@ var type = typeof num2
 // var username = "tommy"
 // console.log(username !== "tommy");
 
-var i = 17
-console.log(i >= 18);
+// var i = 17
+// console.log(i >= 18);
+
+
+var num = 21 / 5
+
+console.log(num); //float integer/number
+console.log(parseInt(num));
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+console.log(Math.random());
+var usern = ["alice", "rob", "jimmy", "cooper", "benjamin", "josh"]
+//TODO pick random person
+const randomindex = Math.random() // 0.4324324234
+const usefulRandomINdex = randomindex * (usern.length - 1)
+const realNumberUsefulIndex = Math.floor(usefulRandomINdex)
+console.log(usern[realNumberUsefulIndex]);
